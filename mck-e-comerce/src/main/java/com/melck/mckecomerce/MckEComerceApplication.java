c6d35308691b60f703432b2a14955eae03e986c2
Melquisedeque <melck_junior@hotmail.com>
2023-01-02 22:31:32 +0000

    First commit

---
+++ mck-e-comerce/src/main/java/com/melck/mckecomerce/MckEComerceApplication.java
@@ -0,0 +1,13 @@
+package com.melck.mckecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MckEComerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MckEComerceApplication.class, args);
+	}
+
+}
---
+++ mck-e-comerce/src/test/java/com/melck/mckecomerce/MckEComerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melck.mckecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MckEComerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
