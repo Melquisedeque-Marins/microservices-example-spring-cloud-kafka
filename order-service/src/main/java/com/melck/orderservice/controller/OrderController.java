6c2e184579a921e34c85ed30a049f674d1a7335c
Melquisedeque <melck_junior@hotmail.com>
2023-01-05 17:07:04 +0000

    OrderRepository, OrderService and OrderController created

--- order-service/src/main/java/com/melck/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/melck/orderservice/controller/OrderController.java
@@ -1,4 +1,23 @@
 package com.melck.orderservice.controller;
 
+import com.melck.orderservice.dto.OrderRequest;
+import com.melck.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/order")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest request) {
+        orderService.placeOrder(request);
+        return "order placed with successfully";
+    }
 }
