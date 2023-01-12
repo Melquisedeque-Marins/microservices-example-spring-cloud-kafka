37af5579347b83cf8de7689152fb91604e159959
Melquisedeque <melck_junior@hotmail.com>
2023-01-12 21:45:13 +0000

    Add discovery client in order and inventory services

--- inventory-service/src/main/java/com/melck/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/melck/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.melck.inventoryservice.repositoty.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
