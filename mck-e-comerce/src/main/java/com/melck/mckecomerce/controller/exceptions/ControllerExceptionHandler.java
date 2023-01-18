90876482cf87f82ceb71847def1c3cf4a8c34f12
Melquisedeque <melck_junior@hotmail.com>
2023-01-18 22:28:05 +0000

    Exception handling to validation exceptions in productController

--- mck-e-comerce/src/main/java/com/melck/mckecomerce/controller/exceptions/ControllerExceptionHandler.java
+++ mck-e-comerce/src/main/java/com/melck/mckecomerce/controller/exceptions/ControllerExceptionHandler.java
@@ -4,14 +4,17 @@ import com.melck.mckecomerce.service.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
 
 @ControllerAdvice
-
 public class ControllerExceptionHandler {
+
+    Instant now = Instant.now();
 
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ObjectNotFoundException e, HttpServletRequest request) {
@@ -21,5 +24,15 @@ public class ControllerExceptionHandler {
         error.setError(e.getMessage());
         error.setPath(request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationException(MethodArgumentNotValidException e, HttpServletRequest request){
+        ValidationError error = new ValidationError(now, HttpStatus.BAD_REQUEST.value(), request.getRequestURI() , "Field validation error - please check the fields above");
+
+        for (FieldError x : e.getBindingResult().getFieldErrors()){
+            error.addErrors(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
