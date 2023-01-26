5c498cc0f387dc5c2c722654dca8498eab4bf89b
Melquisedeque <melck_junior@hotmail.com>
2023-01-26 21:59:38 +0000

    enviroment variables externalization

--- discovery-service/src/main/java/com/melck/discoveryservice/config/SecurityConfig.java
+++ discovery-service/src/main/java/com/melck/discoveryservice/config/SecurityConfig.java
@@ -1,20 +1,29 @@
 package com.melck.discoveryservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${eureka.username}")
+    private String username;
+
+    @Value("${eureka.password}")
+    private String password;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-        .withUser("eureka")
-        .password("password")
+        .passwordEncoder(NoOpPasswordEncoder.getInstance())
+        .withUser(username)
+        .password(password)
         .authorities("USER");
 
     }
