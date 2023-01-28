f63f93647224565cc30c82d858cd27ebbef04729
Melquisedeque <melck_junior@hotmail.com>
2023-01-28 22:43:57 +0000

    Add retry into orderService and add thread into inventoryService

--- inventory-service/src/main/java/com/melck/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/melck/inventoryservice/service/InventoryService.java
@@ -3,6 +3,9 @@ package com.melck.inventoryservice.service;
 import com.melck.inventoryservice.dto.InventoryResponse;
 import com.melck.inventoryservice.repositoty.InventoryRepository;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +13,17 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("wait started");
+        Thread.sleep(10000);
+        log.info("wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
                 .map(inventory -> InventoryResponse.builder()
