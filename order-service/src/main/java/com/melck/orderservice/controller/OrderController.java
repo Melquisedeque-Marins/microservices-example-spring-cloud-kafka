21479a0fb52559c27a95f3e51721d49c60847aac
Melquisedeque <melck_junior@hotmail.com>
2023-01-30 11:46:12 +0000

    Add sleuth and zipkin dependencies

--- order-service/src/main/java/com/melck/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/melck/orderservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.melck.orderservice.dto.OrderRequest;
 import com.melck.orderservice.model.Order;
 import com.melck.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 
@@ -25,6 +26,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
     public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
